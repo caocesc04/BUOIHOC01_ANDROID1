385bfb130b40276711b26fe27aa41435edd6cd9e
caocesc04 <nvcars04@gmail.com>
2017-05-26 21:29:20 +0700

    Initial commit

---
+++ app/src/main/java/demo/dinhconghome/nvcar/buoihoc01_android/MainActivity.java
@@ -0,0 +1,13 @@
+package demo.dinhconghome.nvcar.buoihoc01_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ex02);
+    }
+}
